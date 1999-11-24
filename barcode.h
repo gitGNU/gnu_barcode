5229aa8f5212d4b7c34f2eb0342bdb1629873a18
rubini <rubini>
1999-11-24 22:51:51 +0000

    ./barcode.h: release 0.90.2

--- barcode.h
+++ barcode.h
@@ -24,8 +24,8 @@
 
 #include <stdio.h>
 
-#define BARCODE_VERSION     "0.90.1" 
-#define BARCODE_VERSION_INT   9001
+#define BARCODE_VERSION     "0.90.2" 
+#define BARCODE_VERSION_INT   9002
 
 /*
  * The object
