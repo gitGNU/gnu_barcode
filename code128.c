1505a4e155e48917810bd1daba23cf9b55b0b9fa
rubini <rubini>
2000-01-26 15:58:03 +0000

    ./code128.c: added full-code128 (and change affiliation)

--- code128.c
+++ code128.c
@@ -1,7 +1,7 @@
 /*
  * code128.c -- encoding for code128 (A, B, C)
  *
- * Copyright (c) 1999 Alessandro Rubini (rubini@prosa.it)
+ * Copyright (c) 1999,2000 Alessandro Rubini (rubini@gnu.org)
  * Copyright (c) 1999 Prosa Srl. (prosa@prosa.it)
  *
  *   This program is free software; you can redistribute it and/or modify
@@ -240,5 +240,259 @@ int Barcode_128c_encode(struct Barcode_Item *bc)
     bc->partial = partial;
     bc->textinfo = textinfo;
 
+    return 0;
+}
+
+/*
+ * generic (full-featured) code128 implementation: it selects between
+ * A, B, C according to the data being encoded. F1, F2, F3, F4 are expressed
+ * using ascii chars 0xc1, 0xc2, 0xc3, 0xc4 (0301, 0302, 0303, 0304).
+ * Char '\0' is expressed by 0x80 (0200).
+ */
+
+int Barcode_128_verify(unsigned char *text)
+{
+    if (!strlen(text))
+	return -1;
+    while (*text && (*text<=0x80 || (*text >= 0xc1 && *text <= 0xc4)))
+	text++;
+    if (*text)
+	return -1; /* unencodable character */
+    return 0; /* ok */
+}
+
+
+/*
+ * These functions are extracted from Barcode_128_encode for clarity.
+ * It deals with choosing the symbols used to represent the text
+ * and returns a dynamic array of integers, terminated by -1.
+ *
+ * The algorithm used in choosing the codes comes from App 2 of
+ * "El Codigo Estandar EAN/UCC 128", courtesy of AECOC, Spain.
+ * Thanks to Dani Pardo for getting permission and giving me a copy
+ * of the document
+ */
+
+#define NEED_CODE_A(c) ((c)<32 || (c)==0x80) 
+#define NEED_CODE_B(c) ((c)>=96 && (c)<128)
+
+static int Barcode_a_or_b(unsigned char *text)
+{
+    for ( ; *text; text++) {
+	if (NEED_CODE_A(*text))
+	    return 'A';
+	if (NEED_CODE_B(*text))
+	    return 'B';
+    }
+    return 0; /* any */
+}
+
+static int Barcode_encode_as(int code, int value)
+{
+    /* code is either 'A' or 'B', and value must be valid */
+    if (value >= 0x20 && value <= 0x5F)
+	return value - 0x20; /* both codes */
+    if (value == 0x80) return 64; /* code A */
+    if (value < 0x20) return value+64; /* code A */
+    if (value >= 0x60) return value - 0x20; /* code B */
+    if (value == 0xC1) return FUNC_1;
+    if (value == 0xC2) return FUNC_2;
+    if (value == 0xC3) return FUNC_3;
+    if (value == 0xC4) { /* F4 */
+	if (code == 'A') return CODE_A;
+	return CODE_B;
+    }
+    /* can't happen */
+    return -1;
+}
+
+static int *Barcode_128_make_array(struct Barcode_Item *bc, int *lptr)
+{
+    int len, *codes;
+    unsigned char *s;
+    int i=0, j, code, checksum;
+
+    /* allocate twice the text length + 5, as this is the worst case */
+    len = 2 * strlen(bc->ascii) + 5;
+    codes = malloc(len * sizeof(int));
+    s = bc->ascii;
+
+    /* choose the starting code */
+    if (isdigit(s[0]) && isdigit(s[1]) && isdigit(s[2]) && isdigit(s[3])) {
+	code = 'C';
+    } else {
+	code = Barcode_a_or_b(s);
+	if (!code) code = 'B'; /* default */
+    }
+    codes[i++] = START_A + code - 'A';
+    
+    for (s = bc->ascii; *s; /* increments are in the loop */) {
+	switch(code) {
+
+	    case 'C':
+		if (s[0] == 0xC1) { /* F1 is valid */
+		    codes[i++] = FUNC_1;
+		    s++;
+		} else if (isdigit(s[0]) && isdigit(s[1])) {
+		    /* encode two digits */
+		    codes[i++] = (s[0]-'0') * 10 + s[1]-'0';
+		    s += 2;
+		} else {
+		    /* change code */
+		    code = Barcode_a_or_b(s);
+		    if (!code) code = 'B';
+		    codes[i++] = (code == 'A') ? CODE_A : CODE_B;
+		}
+		break;
+		    
+	    case 'B':
+	    case 'A':
+		for (j=0; isdigit(s[j]); j++)
+		    ;
+		if (j>=4) { /* if there are 4 or more digits, turn to C */
+		    if (j&1) {
+			/* odd number: encode one first */
+			codes[i++] = *(s++) - ' ';
+		    }
+		    codes[i++] = CODE_C;
+		    code = 'C';
+		} else if (code == 'A' && NEED_CODE_B(*s)) {
+		    /* check whether we should use SHIFT or change code */
+		    j = Barcode_a_or_b(s+1);
+		    if (j == 'B') {
+			codes[i++] = CODE_B;
+			code = 'B';
+		    } else {
+			codes[i++] = SHIFT;
+			codes[i++] = Barcode_encode_as('B', *s);
+			s++;
+		    }
+		} else if (code == 'B' && NEED_CODE_A(*s)) {
+		    /* check whether we should use SHIFT or change code */
+		    j = Barcode_a_or_b(s+1);
+		    if (j == 'A') {
+			codes[i++] = CODE_A;
+			code = 'A';
+		    } else {
+			codes[i++] = SHIFT;
+			codes[i++] = Barcode_encode_as('A', *s);
+			s++;
+		    }
+		} else {
+		    codes[i++] = Barcode_encode_as(code, *s);
+		    s++;
+		}
+		break;
+	}	    
+    }
+    /* add the checksum */
+    checksum = codes[0];
+    for (j=1; j<i; j++)
+	checksum += j * codes[j];
+    checksum %= 103;
+    codes[i++] = checksum;
+    codes[i++] = STOP;
+    if (i > len) {
+	fprintf(stderr, "impossible: length is > allocated len\n");
+    }
+    *lptr = i;
+    return codes;
+}
+
+/*
+ * Encoding is tricky, in that we cannot print the ascii
+ * representation in an easy way. Therefore, just skip non-printable
+ * chars and try to fit the rest somehow.
+ */
+int Barcode_128_encode(struct Barcode_Item *bc)
+{
+    static char *text;
+    static char *partial;  /* dynamic */
+    static char *textinfo; /* dynamic */
+    char *textptr;
+    int *codes; /* dynamic */
+    int i, c, len;
+    double textpos, size, step;
+
+    if (bc->partial)
+	free(bc->partial);
+    if (bc->textinfo)
+	free(bc->textinfo);
+    bc->partial = bc->textinfo = NULL; /* safe */
+
+    if (!bc->encoding)
+	bc->encoding = strdup("code 128");
+
+    text = bc->ascii;
+    if (!text) {
+        bc->error = EINVAL;
+        return -1;
+    }
+
+    codes = Barcode_128_make_array(bc, &len);
+    if (!codes) return -1;
+
+    for (i=0; i<len; i++)
+	fprintf(stderr, "%i\n", codes[i]);
+
+    /* the partial code is 6*codelen + ini + term (+margin) */
+    partial = malloc( 6 * len + 4);
+    if (!partial) {
+        bc->error = errno;
+	free(codes);
+        return -1;
+    }
+
+    /* the text information is at most "nnn.n:ff.f:c " * strlen +term */
+    textptr = textinfo = malloc(13*strlen(text) + 2);
+    if (!textinfo) {
+        bc->error = errno;
+        free(partial);
+	free(codes);
+        return -1;
+    }
+
+    /* up to now, it was almost the same code as other encodings */
+
+    strcpy(partial, "0"); /* the first space */
+    for (i=0; i<len; i++) /* the symbols */
+	strcat(partial, codeset[codes[i]]);
+
+    /* avoid bars that fall lower than other bars */
+    for (i=0; partial[i]; i++)
+	if (isalpha(partial[i]))
+	    partial[i] += '1' - 'a';
+
+    /*
+     * the accompanying text: reserve a space for every char.
+     * F[1-4] are rendered as spaces (separators), other unprintable chars
+     * are rendered as underscoress (placeholders)
+     */
+
+    /*
+     * A size of 9 is good for code-C (used above), so 18 for each symbol,
+     * but we place an upper limit of 12 to avoid overlapping on the bars.
+     */
+    size = (int)(180.0 * (len-3) / strlen(text) + .5) / 10.0;
+    if (size > 12.0) size = 12.0;
+
+    /* align the text to the left, using its natural size */
+    step = (int)(10 * (size/18.0 * SYMBOL_WID) + .5) / 10.0;
+    textpos = SYMBOL_WID;
+    fprintf(stderr, "size %g, step %g\n", size, step);
+
+    for (i=0; i<strlen(text); i++) {
+	c = text[i];
+	if (c < 32 || c == 0x80) c = '_'; /* not printable */
+	if (c > 0xc0) c = ' '; /* F code */
+        sprintf(textptr, "%g:%g:%c ", textpos, size, c);
+        textptr += strlen(textptr);
+        textpos += step;
+    }
+    fprintf(stderr, "text %s\n", textinfo);
+
+    bc->partial = partial;
+    bc->textinfo = textinfo;
+    free(codes);
     return 0;
 }
